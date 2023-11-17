e3b4d0839401daa77f72450cb92eceaa7269f46d
ER <77330412+ertor031@users.noreply.github.com>
2023-11-17 20:14:41 +0200

    UpdateAdd

--- app.js
+++ app.js
@@ -9,44 +9,68 @@ let item = "";
 const itemsContainer = document.getElementById("items-container");
 const userCard = document.getElementById("usercard");
 
+
+
 const products = [
-	{ id: "1", imgSrc: "png/Traktors.png", buttonText: "Добавить" },
-	{ id: "2", imgSrc: "png/Traktors.png", buttonText: "Добавить" },
-	{ id: "3", imgSrc: "png/Traktors.png", buttonText: "Добавить" },
-	{ id: "4", imgSrc: "png/Traktors.png", buttonText: "Добавить" },
-	{ id: "5", imgSrc: "png/Traktors.png", buttonText: "Добавить" },
-	{ id: "6", imgSrc: "png/Traktors.png", buttonText: "Добавить" },
+	{ id: "1", name: "Jordan", imgSrc: "png/Traktors.png", buttonText: "Добавить", category: "category2" },
+	{ id: "2", name: "Nike", imgSrc: "png/Traktors.png", buttonText: "Добавить", category: "category2" },
+	{ id: "3", name: "Adidas", imgSrc: "png/Traktors.png", buttonText: "Добавить", category: "category1" },
+	{ id: "4", name: "Nike", imgSrc: "png/Traktors.png", buttonText: "Добавить", category: "category2" },
+	{ id: "5", name: "KC",imgSrc: "png/Traktors.png", buttonText: "Добавить", category: "category1" },
+	{ id: "6", name: "Nike", imgSrc: "png/Traktors.png", buttonText: "Добавить", category: "category2" },
 ];
 
+function filterItems(category) {
+    console.log(category); // Добавьте эту строку для отладки
+
+    const items = document.querySelectorAll('.item');
+    items.forEach(item => {
+        if (category === 'all' || item.classList.contains(category)) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+}
+
 products.forEach(product => {
-	const itemDiv = document.createElement("div");
-	itemDiv.classList.add("item");
+    const itemDiv = document.createElement("div");
+    itemDiv.classList.add("item");
+    itemDiv.classList.add(product.category); // Добавляем класс категории
 
-	const img = document.createElement("img");
-	img.src = product.imgSrc;
-	img.alt = "";
-	img.classList.add("img");
+    const img = document.createElement("img");
+    img.src = product.imgSrc;
+    img.alt = "";
+    img.classList.add("img");
 
-	const button = document.createElement("button");
-	button.classList.add("btn");
-	button.id = `btn${product.id}`;
-	button.innerText = product.buttonText;
+    const productName = document.createElement("p");
+    productName.innerText = product.name;
+    productName.classList.add("productName"); // Добавляем класс productName
 
-	itemDiv.appendChild(img);
-	itemDiv.appendChild(button);
+    const button = document.createElement("button");
+    button.classList.add("btn");
+    button.id = `btn${product.id}`;
+    button.innerText = product.buttonText;
 
-	itemsContainer.appendChild(itemDiv);
+    itemDiv.appendChild(img);
+    itemDiv.appendChild(productName); // Добавляем название под картинкой
+    itemDiv.appendChild(button);
 
-	button.addEventListener("click", function() {
-		if (tg.MainButton.isVisible) {
-			tg.MainButton.hide();
-		} else {
-			tg.MainButton.setText(`Вы выбрали товар ${product.id}!`);
-			item = product.id;
-			tg.MainButton.show();
-		}
-	});
+    itemsContainer.appendChild(itemDiv);
+
+    button.addEventListener("click", function() {
+        if (tg.MainButton.isVisible) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.setText(`Вы выбрали товар ${product.productName}!`);
+            item = product.id;
+            tg.MainButton.show();
+        }
+    });
 });
+
+
+
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
 	tg.sendData(item);
